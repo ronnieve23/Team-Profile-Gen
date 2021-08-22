551d568accdea29c3cbaede4039a0f6ef8d03f1e
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-08-22 13:31:45 -0400

    Finished Testing and Creating Employee Class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('@jest/globals');
+const Employee = require ('../lib/Employee');
+
+test ('this creates an Employee object', () => {
+    const employee =  new Employee('John', 3456, 'employee@email.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test ('test employee class functions', () => {
+    const employee =  new Employee('John', 3456, 'employee@email.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+    expect(employee.getId()).toEqual(expect.any(Number));
+    expect(employee.getName()).toEqual(expect.any(String));
+    expect(employee.getRole()).toBe('Employee');
+
+});
